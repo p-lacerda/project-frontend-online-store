f556a6695b3bea557b3f8a72799ceff4d7831e64
Samuel Nascimento <sfsnascimento@gmail.com>
2021-09-23 17:39:42 -0300

    permitindo manipulçao de quantidade no carrinho de compras

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -6,8 +6,69 @@ import LeftArrow from '../services/image/LeftArrow.svg';
 import BoxOpen from '../services/image/package-box-open.svg';
 
 class Cart extends React.Component {
+  constructor() {
+    const cartStorage = JSON.parse(localStorage.getItem('carrinho'));
+    super();
+    this.state = {
+      storage: cartStorage,
+    };
+    this.removeItem = this.removeItem.bind(this);
+    this.addCartItem = this.addCartItem.bind(this);
+    this.remove = this.remove.bind(this);
+    this.productQuantity = this.productQuantity.bind(this);
+  }
+
+  addCartItem(product) {
+    const { storage } = this.state;
+    const findItem = storage.find((cart) => cart.id === product.id);
+    if (findItem) {
+      const newCart = storage.map((item) => (
+        item.id === product.id
+          ? { ...findItem, quantity: findItem.quantity + 1 } : item));
+      this.setState({
+        storage: newCart,
+      });
+      localStorage.setItem('carrinho', JSON.stringify([...storage]));
+    } else {
+      localStorage
+        .setItem('carrinho', JSON.stringify([...storage, { ...product, quantity: 1 }]));
+    }
+  }
+
+  remove(product) {
+    const { storage } = this.state;
+    const findItem = storage.find((item) => item.id === product.id);
+    if (findItem !== undefined && findItem.quantity === 1) {
+      const newCart = storage.filter((item) => item.id === product.id);
+      localStorage.setItem('carrinho', JSON.stringify([...newCart]));
+    } else {
+      const newCart = storage.map((item) => (
+        item.id === product.id
+          ? { ...findItem, quantity: findItem.quantity - 1 } : item));
+      this.setState({
+        storage: newCart,
+      });
+      localStorage.setItem('carrinho', JSON.stringify([...storage]));
+    }
+  }
+
+  removeItem(product) {
+    const { storage } = this.state;
+    const findItem = storage.filter((cart) => cart.id !== product.id);
+    this.setState({
+      storage: findItem,
+    });
+    localStorage.setItem('carrinho', JSON.stringify(findItem));
+  }
+
+  productQuantity() {
+    const { storage } = this.state;
+    const quant = storage.reduce((quantidade, soma) => quantidade + soma.quantity, 0);
+    return quant;
+  }
+
   render() {
-    const storage = JSON.parse(localStorage.getItem('carrinho'));
+    const { storage } = this.state;
     return (
       <section>
         <Link to="/">
@@ -17,19 +78,49 @@ class Cart extends React.Component {
           <img src={ ShoppingCart } alt="shopping cart" />
         </Link>
         <img src={ BoxOpen } alt="Box Open Empty" />
-        <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
+        {storage.length === 0
+          && <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>}
         <ul>
           {storage.map((product) => (
-            <li
-              key={ product.id }
-              data-testid="shopping-cart-product-name"
-            >
-              { product.title }
-            </li>))}
+            <div key={ product.id } data-testid="shopping-cart-product-name">
+              <img src={ product.thumbnail } alt={ product.title } width="150" />
+              <p>{ product.title }</p>
+              <button
+                type="button"
+                data-testid="product-decrease-quantity"
+                onClick={ () => this.remove(product) }
+              >
+                -
+              </button>
+              <h5 data-testid="shopping-cart-product-quantity">{ product.quantity }</h5>
+              <button
+                type="button"
+                data-testid="product-increase-quantity"
+                onClick={ () => this.addCartItem(product) }
+              >
+                +
+              </button>
+              <div>
+                <button
+                  type="button"
+                  onClick={ () => this.removeItem(product) }
+                >
+                  X
+                </button>
+              </div>
+              <hr />
+            </div>
+          ))}
         </ul>
-        <p data-testid="shopping-cart-product-quantity">{storage.length}</p>
+        <p
+          data-testid="shopping-cart-product-quantity"
+        >
+          Total de produtos:
+          { this.productQuantity() }
+        </p>
       </section>
     );
   }
 }
+
 export default Cart;
